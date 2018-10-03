ceef41a524c257b24689b13440d63b837bef3504
martins3 <979364739@qq.com>
2018-10-03 13:15:13 +0800

    what a stupid thing

---
+++ uva/contest/10/filename.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+
+directory = "/home/shen/Core/ACM/uva/contest/12/"
+
+def process_directory(root):
+    for item in os.listdir(root):
+        if os.path.isdir(item):
+            print("is directory", item)
+            process_directory(item)
+        else:
+            print(item, end=" ---> ")
+            new_name = re.sub(r"UVa*", "", item)
+            if(new_name != item):
+                print(new_name)
+                os.rename(item, new_name)
+
+process_directory(directory)
